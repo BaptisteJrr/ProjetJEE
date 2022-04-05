cb0ff5602d376354b3525ada707be9bd21431950
DESKTOP-NAGBP0N\Baptiste <baptiste.jarry@toulouse.miage.fr>
2022-04-05 17:34:08 +0200

    Métier Panier + Métier Produit

--- ProjetJEE/ProjetJEE-ejb/src/main/java/com/jin/baptiste/company/metier/MetierPanier.java
+++ ProjetJEE/ProjetJEE-ejb/src/main/java/com/jin/baptiste/company/metier/MetierPanier.java
@@ -5,6 +5,10 @@
  */
 package com.jin.baptiste.company.metier;
 
+import com.jin.baptiste.company.entities.Panier;
+import com.jin.baptiste.company.facade.PanierFacadeLocal;
+import com.jin.baptiste.company.facade.ProduitFacadeLocal;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 /**
@@ -14,34 +18,41 @@ import javax.ejb.Stateless;
 @Stateless
 public class MetierPanier implements MetierPanierLocal {
 
+    @EJB
+    private PanierFacadeLocal panierFacade;
+    
+    
+
     @Override
     public void payer(long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        this.panierFacade.payer(idPanier);
     }
 
     @Override
     public void livrer(long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.panierFacade.livrer(idPanier);
     }
 
     @Override
     public void ajouterProduit(long idProduit, long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.panierFacade.ajouterProduit(idProduit, idPanier);
     }
 
     @Override
     public void retirerProduit(long idProduit, long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.panierFacade.retirerProduit(idProduit, idPanier);
     }
 
     @Override
     public void retirerAllProduit(long idProduit, long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       this.panierFacade.retirerAllProduit(idProduit, idPanier);
     }
 
     @Override
     public void supprimerPanier(long idPanier) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Panier p = this.panierFacade.find(idPanier);
+        this.panierFacade.remove(p);
     }
 
     // Add business logic below. (Right-click in editor and choose
