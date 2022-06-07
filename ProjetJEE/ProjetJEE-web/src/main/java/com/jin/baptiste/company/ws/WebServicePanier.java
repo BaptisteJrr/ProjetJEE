2c51c57668c8e23c1e6cd84d6c5908b779d012dc
DESKTOP-NAGBP0N\Baptiste <baptiste.jarry@toulouse.miage.fr>
2022-06-07 15:34:56 +0200

    Gestion d'erreur remonté dans le ws (visible sur soap )

--- ProjetJEE/ProjetJEE-web/src/main/java/com/jin/baptiste/company/ws/WebServicePanier.java
+++ ProjetJEE/ProjetJEE-web/src/main/java/com/jin/baptiste/company/ws/WebServicePanier.java
@@ -7,6 +7,18 @@ package com.jin.baptiste.company.ws;
 
 import com.jin.baptiste.company.projetjeeshared.utilities.TypeProduitEnum;
 import com.jin.baptiste.company.exposition.ExpoLegPanierLocal;
+import com.jin.baptiste.company.projetjeeshared.Exception.ClientInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteSoldeNegaException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteSommeNegaException;
+import com.jin.baptiste.company.projetjeeshared.Exception.PanierAlreadyLivreException;
+import com.jin.baptiste.company.projetjeeshared.Exception.PanierAlreadyPayeException;
+import com.jin.baptiste.company.projetjeeshared.Exception.PanierEmptyException;
+import com.jin.baptiste.company.projetjeeshared.Exception.PanierInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.PanierNoAccountLinkedToClientException;
+import com.jin.baptiste.company.projetjeeshared.Exception.ProduitInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.ProduitQuantiteNegativeException;
+import com.jin.baptiste.company.projetjeeshared.Exception.ProduitStockInsuffisantException;
 import com.jin.baptiste.company.projetjeeshared.utilities.PanierExport;
 import com.jin.baptiste.company.projetjeeshared.utilities.ProduitExport;
 import java.util.List; 
@@ -28,7 +40,7 @@ public class WebServicePanier {
     // "Web Service > Add Operation"
 
     @WebMethod(operationName = "getProduit")
-    public ProduitExport getProduit(@WebParam(name = "idProduit") Long idProduit) {
+    public ProduitExport getProduit(@WebParam(name = "idProduit") Long idProduit) throws ProduitInconnuException {
         return ejbRef.getProduit(idProduit);
     }
 
@@ -43,43 +55,43 @@ public class WebServicePanier {
     }
 
     @WebMethod(operationName = "getPanier")
-    public PanierExport getPanier(@WebParam(name = "idPanier") Long idPanier) {
+    public PanierExport getPanier(@WebParam(name = "idPanier") Long idPanier) throws PanierInconnuException {
         return ejbRef.getPanier(idPanier);
     }
 
     @WebMethod(operationName = "payerPanier")
     @Oneway
-    public void payerPanier(@WebParam(name = "idPanier") Long idPanier) {
+    public void payerPanier(@WebParam(name = "idPanier") Long idPanier) throws PanierInconnuException, PanierEmptyException, CompteSoldeNegaException, CompteInconnuException, CompteSommeNegaException, PanierNoAccountLinkedToClientException, ProduitInconnuException, ProduitQuantiteNegativeException, ProduitStockInsuffisantException {
         ejbRef.payerPanier(idPanier);
     }
 
     @WebMethod(operationName = "ajouterProduitToAClient")
     @Oneway
-    public void ajouterProduitToClient(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idClient") Long idClient) {
+    public void ajouterProduitToClient(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idClient") Long idClient) throws ClientInconnuException, ProduitInconnuException {
         ejbRef.ajouterProduitToClient(idProduit, idClient);
     }
 
     @WebMethod(operationName = "retirerProduit")
     @Oneway
-    public void retirerProduit(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idPanier") Long idPanier) {
+    public void retirerProduit(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idPanier") Long idPanier) throws PanierInconnuException, ProduitInconnuException, PanierAlreadyPayeException, PanierAlreadyLivreException {
         ejbRef.retirerProduit(idProduit, idPanier);
     }
 
     @WebMethod(operationName = "retirerAllProduit")
     @Oneway
-    public void retirerAllProduit(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idPanier") Long idPanier) {
+    public void retirerAllProduit(@WebParam(name = "idProduit") Long idProduit, @WebParam(name = "idPanier") Long idPanier) throws PanierInconnuException, ProduitInconnuException, PanierAlreadyPayeException, PanierAlreadyLivreException {
         ejbRef.retirerAllProduit(idProduit, idPanier);
     }
 
     @WebMethod(operationName = "supprimerPanier")
     @Oneway
-    public void supprimerPanier(@WebParam(name = "idPanier") String idPanier) {
+    public void supprimerPanier(@WebParam(name = "idPanier") String idPanier) throws PanierInconnuException, PanierAlreadyPayeException, PanierAlreadyLivreException {
         Long idPanierL = Long.parseLong(idPanier);
         ejbRef.supprimerPanier(idPanierL);
     }
 
     @WebMethod(operationName = "getPanierActif")
-    public PanierExport getPanierActif(@WebParam(name = "idClient") String idClient) {
+    public PanierExport getPanierActif(@WebParam(name = "idClient") String idClient) throws ClientInconnuException {
         Long idClientL = Long.parseLong(idClient);
         return ejbRef.getPanierActif(idClientL);
     }
