d597c79820dc3428772cc8461acbcd541337c416
GinnWG <wj19930703@gmail.com>
2022-06-19 11:05:16 +0200

    Exception Client

--- ProjetJEEShared/src/main/java/com/jin/baptiste/company/exposition/ExpoLrdRemote.java
+++ ProjetJEEShared/src/main/java/com/jin/baptiste/company/exposition/ExpoLrdRemote.java
@@ -5,6 +5,13 @@
  */
 package com.jin.baptiste.company.exposition;
 
+import com.jin.baptiste.company.projetjeeshared.Exception.ClientCompteAlreadyLinkedException;
+import com.jin.baptiste.company.projetjeeshared.Exception.ClientInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteInconnuException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteSoldeNegaException;
+import com.jin.baptiste.company.projetjeeshared.Exception.CompteSommeNegaException;
+import com.jin.baptiste.company.projetjeeshared.Exception.EmptyFieldException;
+import com.jin.baptiste.company.projetjeeshared.Exception.FormatInvalideException;
 import com.jin.baptiste.company.projetjeeshared.utilities.Position;
 import javax.ejb.Remote;
 
@@ -15,12 +22,12 @@ import javax.ejb.Remote;
 @Remote
 public interface ExpoLrdRemote {
     
-    public void creerCompte(Double solde, String mail);
-    public void crediter(Long id, Double somme);
-    public void debiter(Long id, Double somme);
-    public Position getCompte(Long idCompte);
+    public void creerCompte(Double solde, String mail) throws EmptyFieldException, FormatInvalideException, ClientInconnuException, ClientCompteAlreadyLinkedException;
+    public void crediter(Long id, Double somme) throws CompteInconnuException, CompteSommeNegaException;
+    public void debiter(Long id, Double somme) throws CompteSoldeNegaException, CompteInconnuException, CompteSommeNegaException;
+    public Position getCompte(Long idCompte) throws CompteInconnuException;
     //Get Client??
     // by mail ou by idClient??
-    public Position getCompteByMail(String mail);
+    public Position getCompteByMail(String mail) throws FormatInvalideException, CompteInconnuException;
     
 }
